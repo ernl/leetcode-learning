ee5477bc4a32403e829e1c33b11d67753385677a
ernl <48961712+ernl@users.noreply.github.com>
2022-07-03 13:52:10 +0200

    letter-combinations-of-a-phone-number2.py

---
+++ py/letter-combinations-of-a-phone-number2.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        mapping = {
+            "2": "abc", 
+            "3": "def", 
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"}
+        output = []
+        
+        def backtrack(i, string):
+            if len(string) == len(digits):
+                output.append(string)
+                return
+            for char in mapping[digits[i]]:
+                backtrack(i+1, string+char)
+        if len(digits) > 0:
+            backtrack(0,"")
+        return output
